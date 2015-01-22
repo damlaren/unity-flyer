b015a6b595fcec528c796ec0859613ecf0098a93
David McLaren <damlaren@gmail.com>
2015-01-22 14:48:24 -0800

    have main menu scene looking nice

---
+++ unity/Assets/AnimateScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimateScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//iTween.MoveBy(GameObject.Find("TitleText"),
+		 //             iTween.Hash("y", 5, "easeType", "easeInOutSine", "loopType",
+		  //          			  "pingpong", "delay", 0.0, "time", 4.0));
+		iTween.MoveBy(GameObject.Find("TitleText"),
+		              iTween.Hash("x", 5, "y", 5, "easeType", "easeInOutSine", "loopType",
+		            "pingpong", "delay", 0.0, "time", 5.0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ unity/Assets/RotationScript.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+	var ROTATION_SPEED : float = 1.0 / 10.0; // speed of rotation in degrees per second
+	transform.Rotate(Vector3.up * Time.deltaTime * ROTATION_SPEED);
+}
