bc39e67d4df347bd30f158550e0ac5ab23a3b8c1
David McLaren <damlaren@gmail.com>
2015-01-23 21:51:20 -0800

    finished scene2, a click-n-drag demo

---
+++ unity/Assets/BirdDragger.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdDragger : MonoBehaviour {
+
+	private Vector3 startPoint;
+	private Vector3 dragOffset;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown() {
+		print ("Mouse down");
+		startPoint = Camera.main.WorldToScreenPoint (gameObject.transform.position);
+		Vector3 screenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, startPoint.z);
+		dragOffset = gameObject.transform.position - Camera.main.ScreenToWorldPoint (screenPoint);
+	}
+
+	void OnMouseDrag() {
+		print ("draggin");
+		Vector3 screenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, startPoint.z);
+		transform.position = Camera.main.ScreenToWorldPoint (screenPoint) + dragOffset;
+	}
+}
