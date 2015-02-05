e4bb9460ab4acb4717f6d00d125b6a890d2b10f0
David McLaren <damlaren@gmail.com>
2015-02-05 13:20:36 -0800

    desired direction force in plugin

--- assignment3/UnityProject/Assets/Scripts/ParticleMotionScript.cs
+++ assignment3/UnityProject/Assets/Scripts/ParticleMotionScript.cs
@@ -5,27 +5,29 @@ using System.Runtime.InteropServices;
 
 public class ParticleMotionScript : MonoBehaviour {
 	float[] particle_states;
-	float[] center;
+	float[] goal_position;
+	float[] weights;
 
 	[DllImport("Assets/Plugins/ParticlePlugin")]
-	private static extern void centralGravity(float[] particle_states,float[] center,float dt);
+	private static extern void flock(float[] particle_states, float[] goal_position,
+	                                 float[] weights, float dt);
 
 	// Use this for initialization
 	void Start () {
 		particle_states = new float[6];
-		center = new float[3];
+		goal_position = new float[3] {25, 8, 0};
+		weights = new float[5] {100, 1, 1, 1, 1};
 
 		////position
-		for(int d=0;d<3;d++) particle_states[d]=transform.position[d];
+		for(int d=0; d < 3;d++) particle_states[d] = transform.position[d];
 		////velocity
-		for(int d=0;d<3;d++) particle_states[d+3]=0;
-		////center
-		for(int d=0;d<3;d++) center[d]=0;
+		for(int d=0; d < 3;d++) particle_states[d + 3]=0;
 	}
 	
 	void FixedUpdate () {
 		float dt = Time.deltaTime;
-		centralGravity (particle_states,center,dt);
-		transform.position = new Vector3 (particle_states[0],particle_states[1],particle_states[2]);
+		flock (particle_states, goal_position, weights, dt);
+		transform.position = new Vector3 (particle_states[0], particle_states[1],
+		                                  particle_states[2]);
 	}
 }
--- assignment3/VSParticlePlugin/ParticlePlugin/particle_movement.cpp
+++ assignment3/VSParticlePlugin/ParticlePlugin/particle_movement.cpp
@@ -1,31 +1,85 @@
 #define EXPORT_API __declspec(dllexport) // Visual Studio needs to annotate exported functions with this
 #include <cmath>
+#include <string.h>
 
 extern "C"
 {
-    const EXPORT_API void centralGravity(float* particle_states,float* center,float dt)
+	/**
+	  * Store normalized vector vin with n entries into vout.
+	  */
+	void normalize(float *vout, float* vin, int n) {
+		float s = 0;
+		for (int d = 0; d < n; d++) {
+			s += vin[d] * vin[d];
+		}
+		float mag = std::sqrt(s);
+		if (mag != 0) {
+			for (int d = 0; d < n; d++) {
+				vout[d] = vin[d] / mag;
+			}
+		}
+	}
+
+	/** Multiply vector v of length n by scalar s, store into vout. */
+	void multiply(float* vout, float* vin, float s, int n) {
+		for (int d = 0; d < n; d++) {
+			vout[d] = vin[d] * s;
+		}
+	}
+
+	/** Store vin + win in vout. v, w must be length n. */
+	void add(float *vout, float *vin, float *win, int n) {
+		for (int d = 0; d < n; d++) {
+			vout[d] = vin[d] + win[d];
+		}
+	}
+
+	/** Store vin + win in vout. v, w must be length n. */
+	void subtract(float *vout, float *vin, float *win, int n) {
+		float wCopy[3];
+		multiply(wCopy, win, -1.0f, n);
+		add(vout, vin, wCopy, n);
+	}
+
+	/** Copy vector vin of length n to vout. */
+//	void copy(float* vout, const float* vin, int n) {
+//		memcpy(vout, vin, n * sizeof(float));
+//	}
+
+	/**
+	  * Model a flock of birds.
+	  * particle_states: [x; v] for this particle.
+	  * goal_position: [x] goal position.
+	  * weights: 5 of them, respectively for: desired motion, separation, alignment, cohesion, avoidance
+	  * dt: timestep
+	  */
+    const EXPORT_API void flock(float* particle_states, float* goal_position, float* weights, float dt)
     {
-        float* x=&particle_states[0];
-        float* v=&particle_states[3];
-        
-        float gravity[3]; //"gravity" here is just a direction from the particle's position to the specified center
-        for(int d=0;d<3;d++){
-            gravity[d]=center[d]-x[d];
-        }
+        float* x = &particle_states[0];
+        float* v = &particle_states[3];
 
-        float magnitude=std::sqrt(gravity[0]*gravity[0] + gravity[1]*gravity[1] + gravity[2]*gravity[2]);
-        float epsilon=1e-10f;
+		// Force 1: Desired motion
+		float currentDirection[3];
+		float desiredDirection[3];
+		float desiredDirectionDifference[3];
+		float desiredDirectionForce[3];
+		float desiredDirectionWeight = weights[0];
+		subtract(desiredDirection, goal_position, x, 3);
+		normalize(currentDirection, v, 3);
+		normalize(desiredDirection, desiredDirection, 3);
+		subtract(desiredDirectionDifference, desiredDirection, currentDirection, 3);
+		multiply(desiredDirectionForce, desiredDirectionDifference, desiredDirectionWeight, 3);
 
-        for(int d=0;d<3;d++){
-            gravity[d]/=(magnitude+epsilon);
-        }
-        
-        for(int d=0;d<3;d++){
-            v[d]+=gravity[d]*dt;
-        }
-        for(int d=0;d<3;d++){
-            x[d]+=v[d]*dt;
-        }
+		// Add all the forces together at the end, transform to impulse.
+		float totalImpulse[3];
+		add(totalImpulse, totalImpulse, desiredDirectionForce, 3);
+		multiply(totalImpulse, totalImpulse, dt, 3);
+
+		// Update velocity and position.
+		float posChange[3];
+		add(v, v, totalImpulse, 3);
+		multiply(posChange, v, dt, 3);
+		add(x, x, posChange, 3);
     }
     
 } // end of export C block
