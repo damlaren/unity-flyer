2fca966bf039b80d3067b32f3278753873002381
David McLaren <damlaren@gmail.com>
2015-02-09 09:55:06 -0800

    bridge supports have HP! and the bridge falls down!

---
+++ unity/Assets/SupportScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SupportScript : MonoBehaviour {
+
+	int hp = 50;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		// reduce hp on collision with a bullet
+		GameObject g = collision.gameObject;
+		if (g.GetComponent<BulletScript> () != null) {
+			hp--;
+			if (hp <= 0) {
+				Destroy(gameObject);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
