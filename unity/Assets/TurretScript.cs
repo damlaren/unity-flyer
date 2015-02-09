fc7b8046184f99806e9a09b2142acf7d95484dca
David McLaren <damlaren@gmail.com>
2015-02-09 11:26:13 -0800

    turrets shoot the player

--- unity/Assets/TurretScript.cs
+++ unity/Assets/TurretScript.cs
@@ -14,11 +14,14 @@ public class TurretScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		//Instantiate (bulletPrefab, transform.position + transform.forward * 10.0f, transform.rotation);
 		Vector3 wayPoint = target.transform.position;
-		wayPoint.y = transform.position.y;
-		Quaternion qTo = Quaternion.LookRotation (wayPoint - transform.position, transform.up);
-		//transform.rotation = Quaternion.RotateTowards (transform.rotation, qTo, rotationSpeed * Time.deltaTime);
-		transform.rotation = Quaternion.Lerp (transform.rotation, qTo, Time.deltaTime * rotationSpeed);
+		Vector3 targetDir = wayPoint - transform.position;
+		targetDir.Normalize ();
+		Quaternion qTo = Quaternion.LookRotation (targetDir, transform.up);
+		transform.rotation = Quaternion.RotateTowards (transform.rotation, qTo, rotationSpeed * Time.deltaTime);
+		//transform.rotation = Quaternion.Lerp (transform.rotation, qTo, Time.deltaTime * rotationSpeed);
+		if ((wayPoint - transform.position).magnitude < 500) {
+			Instantiate (bulletPrefab, transform.position + targetDir * 10.0f, transform.rotation);
+		}
 	}
 }
